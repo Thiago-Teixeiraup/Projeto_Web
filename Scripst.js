3c60dfd4efdcd85c8688406be22185896b8aed58
Thiago-Teixeiraup <122649685+Thiago-Teixeiraup@users.noreply.github.com>
2024-01-17 20:03:01 -0300

    Recursos JS
    Trabalhando no Menu!
    alteracoes CSS
    HTML e
    JavaScript foram feitas.
    incluindo um layout diferente e automatizando recursos.
    proximos passos serao o incremento do conteudo da pag.

--- Scripst.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.getElementById('hamburger');
-
-function toggleMenu() {
-    const nav = document.getElementById('nav');
-    nav.classList.toggleMenu('actve');
-}
-
-hamburger.addEventListener('click', toggleMenu);
